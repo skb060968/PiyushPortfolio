700d4ce952c5e565b202335b917ad3cd179a588b
Sunil <skb060968@gmail.com>
2026-01-17 00:24:17 +0530

    image fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Keep defaults — no export, no basePath, no assetPrefix
-  // This works for:
-  // • Local dev
-  // • Vercel
-  // • Netlify
-  // • Custom domains
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
